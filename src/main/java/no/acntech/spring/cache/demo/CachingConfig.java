fa6107afa546783fefefe9173fca34647a908845
Ismar Slomic <ismar.slomic@vegvesen.no>
2019-02-07 22:38:56 +0100

    Moved cache configuration to separate class

---
+++ src/main/java/no/acntech/spring/cache/demo/CachingConfig.java
@@ -0,0 +1,25 @@
+package no.acntech.spring.cache.demo;
+
+import java.util.Collections;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CachingConfig {
+
+    public static final String USERS_CACHE = "Users";
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(Collections.singletonList(
+                new ConcurrentMapCache(USERS_CACHE)));
+        return cacheManager;
+    }
+}
--- src/main/java/no/acntech/spring/cache/demo/web/UsersResource.java
+++ src/main/java/no/acntech/spring/cache/demo/web/UsersResource.java
@@ -3,8 +3,9 @@ package no.acntech.spring.cache.demo.web;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
@@ -15,25 +16,32 @@ import org.springframework.web.bind.annotation.RestController;
 import no.acntech.spring.cache.demo.domain.User;
 import no.acntech.spring.cache.demo.service.UserService;
 
+import static no.acntech.spring.cache.demo.CachingConfig.USERS_CACHE;
+
 @RestController
 @RequestMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
-@CacheConfig(cacheNames = "users")
 public class UsersResource {
 
     @Autowired
     private UserService userService;
 
-    @Cacheable
+    private static final Logger logger = LoggerFactory.getLogger(UsersResource.class);
+
+
+    @Cacheable(value = USERS_CACHE)
     @RequestMapping(method = RequestMethod.GET)
     public List<User> getUsers() {
+        logger.info("Calling external user services");
+
         List<String> namesForSlowService = Arrays.asList("Tom", "Jon", "Peter");
-
         List<String> namesForSuperSlowService = Arrays.asList("Anna", "Isabelle", "Lizzi");
+        List<User> users = userService.getUsers(namesForSlowService, namesForSuperSlowService);
 
-        return userService.getUsers(namesForSlowService, namesForSuperSlowService);
+        logger.info("Returning list of users response");
+        return users;
     }
 
-    @CacheEvict(value = "users", allEntries = true)
+    @CacheEvict(value = USERS_CACHE, allEntries = true)
     @RequestMapping(method = RequestMethod.DELETE)
     public void clearUsersCache() {
 
